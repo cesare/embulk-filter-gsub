dd5f21c6f6f7eb83bdf76dac8aa742baf078bbae
SAWADA Tadashi <cesare@mayverse.jp>
2018-01-14 14:02:12 +0900

    Fix factory methods

--- src/main/kotlin/org/embulk/filter/gsub/ColumnReplacerFactory.kt
+++ src/main/kotlin/org/embulk/filter/gsub/ColumnReplacerFactory.kt
@@ -4,16 +4,15 @@ import org.embulk.filter.gsub.SubstitutionRule.SubstitutionType
 import org.embulk.filter.gsub.replacer.*
 
 class ColumnReplacerFactory {
-    fun create(columns: List<TargetColumnConfig>): Map<String, TextReplacer> {
-        val map = HashMap<String, TextReplacer>()
-        columns.forEach { column -> map.put(column.name, createReplacerForColumn(column)) }
-        return map
+    fun create(task: GsubFilterPlugin.PluginTask): Map<String, TextReplacer> {
+        return task.targetColumns.mapValues { column ->
+            createReplacerForRules(column.value)
+        }
     }
 
-    fun createReplacerForColumn(config: TargetColumnConfig): TextReplacer {
-        val replacers = ArrayList<TextReplacer>()
-        val combinedReplacer = CombinedReplacer(replacers)
-        return combinedReplacer
+    private fun createReplacerForRules(rules: List<SubstitutionRule>): TextReplacer {
+        val replacers = rules.map { createReplacer(it) }
+        return CombinedReplacer(replacers)
     }
 
     private fun createReplacer(rule: SubstitutionRule): TextReplacer {
