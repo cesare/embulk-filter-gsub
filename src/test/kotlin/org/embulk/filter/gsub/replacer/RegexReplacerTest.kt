d8113c0046f58d5ccd460c0a9d74b847c0a9ce5b
SAWADA Tadashi <cesare@mayverse.jp>
2018-02-12 15:04:25 +0900

    Add tests for regular expression options

--- src/test/kotlin/org/embulk/filter/gsub/replacer/RegexReplacerTest.kt
+++ src/test/kotlin/org/embulk/filter/gsub/replacer/RegexReplacerTest.kt
@@ -9,15 +9,119 @@ import org.junit.Test
 class RegexReplacerTest {
     @Test
     fun testExecute() {
-        val regexOptions = object: RegexOptions {
+        val regexOptions = createRegexpOption()
+
+        val factory = RegexFactory()
+        val pattern = factory.create("(\\w*):\\s*(.*)", regexOptions)
+
+        val replacer = RegexReplacer(pattern, "$1 [$2]")
+        val result = replacer.execute("test: foo bar baz")
+        Assert.assertEquals("test [foo bar baz]", result)
+    }
+
+    @Test
+    fun testExecuteWithoutIgnoreCaseOption() {
+        val regexOptions = createRegexpOption(ignoreCase = false)
+
+        val factory = RegexFactory()
+        val pattern = factory.create("foo", regexOptions)
+
+        val replacer = RegexReplacer(pattern, "*test-foo*")
+
+        Assert.assertEquals("*test-foo* bar baz", replacer.execute("foo bar baz"))
+        Assert.assertEquals("Foo bar baz", replacer.execute("Foo bar baz"))
+        Assert.assertEquals("FOO bar baz", replacer.execute("FOO bar baz"))
+    }
+
+    @Test
+    fun testExecuteWithIgnoreCaseOption() {
+        val regexOptions = createRegexpOption(ignoreCase = true)
+        val factory = RegexFactory()
+        val pattern = factory.create("foo", regexOptions)
+
+        val replacer = RegexReplacer(pattern, "*test-foo*")
+
+        Assert.assertEquals("*test-foo* bar baz", replacer.execute("foo bar baz"))
+        Assert.assertEquals("*test-foo* bar baz", replacer.execute("Foo bar baz"))
+        Assert.assertEquals("*test-foo* bar baz", replacer.execute("FOO bar baz"))
+    }
+
+    @Test
+    fun testExecuteWithoutMultilineOption() {
+        val regexOptions = createRegexpOption(multiline = false)
+        val factory = RegexFactory()
+        val pattern = factory.create("^bar", regexOptions)
+
+        val replacer = RegexReplacer(pattern, "*BAR*")
+
+        Assert.assertEquals("foo\nbar\nbaz", replacer.execute("foo\nbar\nbaz"))
+    }
+
+    @Test
+    fun testExecuteWithMultilineOption() {
+        val regexOptions = createRegexpOption(multiline = true)
+        val factory = RegexFactory()
+        val pattern = factory.create("^bar", regexOptions)
+
+        val replacer = RegexReplacer(pattern, "*BAR*")
+
+        Assert.assertEquals("foo\n*BAR*\nbaz", replacer.execute("foo\nbar\nbaz"))
+    }
+
+    @Test
+    fun testExecuteWithoutDotMatchesAllOption() {
+        val regexOptions = createRegexpOption(dotMatchesAll = false)
+        val factory = RegexFactory()
+        val pattern = factory.create("foo.bar.baz", regexOptions)
+
+        val replacer = RegexReplacer(pattern, "[foo-bar-baz]")
+
+        Assert.assertEquals("[foo-bar-baz]", replacer.execute("foo/bar/baz"))
+        Assert.assertEquals("foo\nbar/baz", replacer.execute("foo\nbar/baz"))
+    }
+
+    @Test
+    fun testExecuteWithDotMatchesAllOption() {
+        val regexOptions = createRegexpOption(dotMatchesAll = true)
+        val factory = RegexFactory()
+        val pattern = factory.create("foo.bar.baz", regexOptions)
+
+        val replacer = RegexReplacer(pattern, "[foo-bar-baz]")
+
+        Assert.assertEquals("[foo-bar-baz]", replacer.execute("foo/bar/baz"))
+        Assert.assertEquals("[foo-bar-baz]", replacer.execute("foo\nbar/baz"))
+    }
+
+    @Test
+    fun testExecuteWithEnableCommentsOption() {
+        val regexOptions = createRegexpOption(enableComments = true)
+        val factory = RegexFactory()
+
+        val patternString = """
+            |(ba\w)    # matches bar and baz
+            """.trimMargin()
+        val pattern = factory.create(patternString, regexOptions)
+
+        val replacer = RegexReplacer(pattern, "*$1*")
+
+        Assert.assertEquals("foo *bar* *baz*", replacer.execute("foo bar baz"))
+    }
+
+    private fun createRegexpOption(
+            ignoreCase:     Boolean = false,
+            multiline:      Boolean = true,
+            dotMatchesAll:  Boolean = false,
+            enableComments: Boolean = false
+    ): RegexOptions {
+        return object: RegexOptions {
             override val ignoreCase: Boolean
-                get() = true
+                get() = ignoreCase
             override val multiline: Boolean
-                get() = true
+                get() = multiline
             override val dotMatchesAll: Boolean
-                get() = false
+                get() = dotMatchesAll
             override val enableComments: Boolean
-                get() = false
+                get() = enableComments
 
             override fun validate() {
             }
@@ -26,12 +130,5 @@ class RegexReplacerTest {
                 throw NotImplementedError()
             }
         }
-
-        val factory = RegexFactory()
-        val pattern = factory.create("(\\w*):\\s*(.*)", regexOptions)
-
-        val replacer = RegexReplacer(pattern, "$1 [$2]")
-        val result = replacer.execute("test: foo bar baz")
-        Assert.assertEquals("test [foo bar baz]", result)
     }
 }
